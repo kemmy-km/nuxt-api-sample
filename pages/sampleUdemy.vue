8f589fbf39a4ab90992793b00c66e97fa37636fa
keigom2020 <qlpt2020@gmail.com>
2022-02-12 13:32:53 +0900

    add:udemyで使用したサンプルのファイルを追加

---
+++ pages/sampleUdemy.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="container">
+    <div>
+      <!-- <p>{{ $store.state.message }}</p> -->
+      <p>{{ $store.state.hello.message }}</p>
+
+      <!-- <button @click="$store.commit('updateMessage')">Update</button> -->
+      <!-- <button @click="$store.commit('updateMessage', 'Commit with payload')">Update</button> -->
+      <!-- <button @click="$store.dispatch('updateMessageAction')">Dispatch</button> -->
+      <!-- <button @click="$store.dispatch('updateMessageAction', 'dispatch with payload')">Dispatch</button> -->
+      <button @click="$store.dispatch('hello/updateMessageAction', 'dispatch with payload')">Dispatch</button>
+
+   </div>
+  </section>
+</template>
+
+<script>
+// import { request } from "../utils/request"
+// import { mapState } from 'vuex'
+// import { createStore } from '../store/index'
+
+export default {
+}
+
+</script>
